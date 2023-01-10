1fc65c205a6fc5f41e9c02b81ddf6e3f45b3bb95
Evgeniy <evgeniy_mq@mail.com>
2023-01-10 19:12:37 +0300

    Number_3

---
+++ Number_3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 21 - HARD необязательная
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние 
+// между ними в N-мерном пространстве. Сначала задается N с клавиатуры, потом задаются координаты точек.
+
+
+Console.WriteLine("Введите мерность пространсва");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] arrayA = new int [N];
+int[] arrayB = new int [N];
+
+Console.WriteLine("Введите координаты точки A, по одному числу, исходя из мерности пространства");
+for (int i = 0; i < N; i++) arrayA[i] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B, по одному числу, исходя из мерности пространства");
+for (int i = 0; i < N; i++) arrayB[i] = Convert.ToInt32(Console.ReadLine());
+
+
+double Magic(int[] pointA, int[] pointB)
+{
+    int D = 0;
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        D = (pointA[i] - pointB[i]) * (pointA[i] - pointB[i]);
+        sum = sum + D; 
+    }
+    double result = Math.Sqrt(sum);
+    return result;
+}
+
+Magic(arrayA, arrayB);
+
+
+// double D = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)); 
+
+// double result = Math.Sqrt((xA - xB)*(xA - xB))
+
+
+
+
