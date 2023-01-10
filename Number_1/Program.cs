75c7473662385344e4ca856f3f79bda16c2def97
Evgeniy <evgeniy_mq@mail.com>
2023-01-10 18:46:53 +0300

    Number_1_1

--- Number_1/Program.cs
+++ Number_1/Program.cs
@@ -3,13 +3,19 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Введите пятизначное число");
-int x = Convert.ToInt32(Console.ReadLine());
+void Magic(int x)
+{
+    int x1 = x / 10000;
+    int x2 = x / 1000 % 10;
+    int x4 = x % 100 / 10;
+    int x5 = x % 10;
+    if (x1 == x5 && x2 == x4) Console.WriteLine("да");
+    else Console.WriteLine("нет");
+}
 
-int x1 = x / 10000;
-int x2 = x / 1000 % 10;
-int x4 = x % 100 / 10;
-int x5 = x % 10;
 
-if (x1 == x5 && x2 == x4) Console.WriteLine("да");
-else Console.WriteLine("нет");
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Magic(number);
+
